38e95c4fe13d1280636684150e61c98db41ef4d4
tharindu432 <tharinduchaturanga432@gmail.com>
2023-09-07 16:27:52 +0530

    login frontend fetch

--- frontend/src/pages/AuthenticationPages/Login.jsx
+++ frontend/src/pages/AuthenticationPages/Login.jsx
@@ -1,4 +1,6 @@
 import React, { useState } from 'react'
+import { useEffect } from 'react';
+import axios from 'axios';
 import './Login.css';
 import TextField from '@mui/material/TextField';
 import AccountCircle from '@mui/icons-material/AccountCircle';
@@ -17,11 +19,26 @@ const Login = () => {
   const [password , setPassword] = useState('');
   const navigate = useNavigate();
 
-  const handleLogin = ()=>{
-    navigate('/user/dashboard');
-  }
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+   console.log(password)
+    try {
+      const response = await axios.post('http://localhost:10000/user/login', {
+        email: email,
+        password: password,
+      });
+      console.log(response.data.password)
 
+      // Reset the form fields
+      // setEmail('');
+      // setPassword('');
 
+      // Redirect to the dashboard
+      navigate('/dashboard');
+    } catch (error) {
+      console.error(error);
+    }
+  };
   return (
     <div className="landingPageContainer mt-5">
       <div className="loginPageContainer d-flex flex-column align-items-center">
@@ -32,18 +49,20 @@ const Login = () => {
         </div>
 
         {/* Button & company Container */}
-        <form action="" className='loginform mt-4'>
+        <form action="" className='loginform mt-4' >
         <Box sx={{ display: 'flex', alignItems: 'flex-end' }} className='mt-3'>
+          
           <AccountCircle sx={{ color: 'action.active', mr: 1, my: 0.5 }} />
-          <TextField fullWidth required label="Enter email" helperText='' variant="standard" onChange={e=>setEmail(e.target.value)}/>
+          
+          <TextField fullWidth required label="Enter email" helperText='' value={email} variant="standard" onChange={e=>setEmail(e.target.value)}/>
         </Box>
         <Box sx={{ display: 'flex', alignItems: 'flex-end' }} className='mt-3'>
           <LockIcon sx={{ color: 'action.active', mr: 1, my: 0.5 }} />
-          <TextField fullWidth required type='password' helperText='' label="Enter password" variant="standard" onChange={e=>setPassword(e.target.value)}/>
+          <TextField fullWidth required type='password' helperText='' value={password} label="Enter password" variant="standard" onChange={e=>setPassword(e.target.value)}/>
         </Box>
 
         <div className="text-content d-flex flex-column align-items-center mt-3">
-        <Button fullWidth className='mb-4 mt-3 bg-primary' variant="contained" size='large' onClick={handleLogin}>Login</Button>
+        <Button fullWidth className='mb-4 mt-3 bg-primary' variant="contained" size='large' onClick={handleSubmit} >Login</Button>
         {/* <Link to={'/googleauth'} className='d-flex align-items-center mb-5'> <GoogleIcon/>Sign in with Google</Link> */}
         <GoogleAuth className='rounded mb-4 mt-3 bg-primary containauth'  />
         <Link className='mb-4' to={'/'}>Forgot Password?</Link>
@@ -57,5 +76,6 @@ const Login = () => {
     </div>
   )
 }
+
 
 export default Login
